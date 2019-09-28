94c63b216d858edadbc5fd1a0da121bf77dd4203
osdiallo <omdiallo@gmail.com>
2019-09-28 14:44:37 -0400

    Added registration logic, switching to main page

--- app/src/main/java/com/example/flydar/ui/login/Registration.java
+++ app/src/main/java/com/example/flydar/ui/login/Registration.java
@@ -19,7 +19,9 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import com.example.flydar.R;
+import com.example.flydar.MainActivity;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -41,9 +43,14 @@ public class Registration extends AppCompatActivity{
         signupPhone = findViewById(R.id.phone_num);
         btnRegister = findViewById(R.id.register_user);
 
+        signupEmail.addTextChangedListener(new viewTextWatcher());
+        signupPass.addTextChangedListener(new viewTextWatcher());
+        signupPhone.addTextChangedListener(new viewTextWatcher());
+
         btnRegister.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
+
                 String emailText = signupEmail.getText().toString();
                 String passText = signupPass.getText().toString();
                 String phoneText = signupPhone.getText().toString();
@@ -64,6 +71,28 @@ public class Registration extends AppCompatActivity{
         });
     }
 
+    private class viewTextWatcher implements TextWatcher{
+        public void beforeTextChanged(CharSequence charSequence, int i1, int i2, int i3){}
+        public void onTextChanged(CharSequence charSequence, int i1, int i2, int i3){
+            checkRequiredFields();
+        }
+        public void afterTextChanged(Editable editable){}
+    }
+
+    private void checkRequiredFields(){
+        String emailField = signupEmail.getText().toString();
+        String passField = signupPass.getText().toString();
+        String phoneField = signupPhone.getText().toString();
+
+        if(!TextUtils.isEmpty(emailField) && !TextUtils.isEmpty(passField)
+                && !TextUtils.isEmpty(phoneField)){
+            btnRegister.setEnabled(true);
+        } else {
+            btnRegister.setEnabled(false);
+        }
+
+    }
+
     private void submitForm(String newEmail, String newPass, String newPhone){
         JSONObject newUser = new JSONObject();
 
@@ -71,6 +100,14 @@ public class Registration extends AppCompatActivity{
             newUser.put("email", newEmail);
             newUser.put("password", newPass);
             newUser.put("phone", newPhone);
+
+            btnRegister.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(i);
+                }
+            });
 
         } catch (JSONException e){
             // catch block
