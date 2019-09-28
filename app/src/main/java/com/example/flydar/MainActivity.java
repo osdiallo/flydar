f37cd2695af7ab70ca4ecfd2300b4cc07e39dae2
Tristan Carter <dmw131@vols.utk.edu>
2019-09-28 15:20:03 -0400

    Added create event page.

--- app/src/main/java/com/example/flydar/MainActivity.java
+++ app/src/main/java/com/example/flydar/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.flydar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.flydar.ui.login.Registration;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button create = findViewById(R.id.create);
+
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), CreatePost.class);
+                startActivity(i);
+            }
+        });
     }
 }
